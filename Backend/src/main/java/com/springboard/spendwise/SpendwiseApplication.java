545b83178e172e30f4e165cfa06250e4022172d6
Samu026 <149374089+Samu026@users.noreply.github.com>
2024-04-17 01:22:06 +0530

    Backend for password forget and reset functionality

---
+++ Backend/src/main/java/com/springboard/spendwise/SpendwiseApplication.java
@@ -0,0 +1,13 @@
+package com.springboard.spendwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendwiseApplication.class, args);
+	}
+
+}
