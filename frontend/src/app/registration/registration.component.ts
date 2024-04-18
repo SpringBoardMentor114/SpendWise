80247d9eb158596116422426473cff504031e290
harshyelwatkar <128224198+harshyelwatkar@users.noreply.github.com>
2024-04-18 14:56:15 +0530

    Added logic for registration form

--- frontend/src/app/registration/registration.component.ts
+++ frontend/src/app/registration/registration.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrl: './registration.component.css'
 })
 export class RegistrationComponent {
+  signUpObj : SignUp = new SignUp();
 
+  onRegister() {
+    const localUser = localStorage.getItem('registered_users');
+    if(localUser != null) {
+      const users = JSON.parse(localUser);
+      users.push(this.signUpObj);
+      localStorage.setItem('registered_users', JSON.stringify(users))
+    } else {
+      const users = [];
+      users.push(this.signUpObj);
+      localStorage.setItem('registered_users', JSON.stringify(users))
+    }
+    alert('Registration Successful')
+  }
+}
+
+export class SignUp {
+  name: string;
+  email:string;
+  password:string;
+  
+
+  constructor() {
+    this.name = "";
+    this.email = "";
+    this.password = "";
+    
+  }
 }
