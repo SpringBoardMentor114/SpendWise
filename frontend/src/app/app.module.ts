f42d96168443d8d42f9ff2d8c9e30a15d3db2af9
Sarvesh patil <69429909+its-a-maniac@users.noreply.github.com>
2024-05-13 18:58:34 +0530

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,7 +13,17 @@ import { ExpenseListComponent } from './expenses/expense-list/expense-list.compo
 import { EditingComponent } from './expenses/editing/editing.component';
 import { FormsModule } from '@angular/forms';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
-
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import { ExpenseManagementDashboardAppComponent } from './expenses/expense-management-dashboard-app/expense-management-dashboard-app.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
 import { ReactiveFormsModule } from '@angular/forms';
 // import { HomeComponent } from './home/home.component'; 
 import { AddComponent } from './expenses/add/add.component';
@@ -31,7 +41,9 @@ const routes: Routes = [
   {path:'Registration',component:RegistrationComponent},
   {path:'PassReset',component:PassresetComponent},
   {path:'Login',component:LoginComponent},
- 
+  {path:'**',component:LoginComponent},
+  {path:'EDIT',component:EditingComponent}
+
   // {path:'Home',component:HomeComponent}
 
 ];
@@ -48,6 +60,7 @@ const routes: Routes = [
     ExpenseListComponent,
     EditingComponent,
     AddComponent,
+   ExpenseManagementDashboardAppComponent,
   ],
   imports: [
     BrowserModule,
@@ -57,6 +70,18 @@ const routes: Routes = [
     RouterModule.forRoot(routes),
     NgxChartsModule,
     FormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSelectModule,
     HttpClientModule
   ],
   providers: [
